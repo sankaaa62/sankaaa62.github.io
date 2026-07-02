c099982b78f45a67442ab9ad8683db50c95dac96
sankaaa62 <kandakov.sasha@gmail.com>
2026-07-02 15:02:31 +0400

    feat: localized page title, sitemap, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://sankaaa62.github.io',
+  integrations: [sitemap()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
